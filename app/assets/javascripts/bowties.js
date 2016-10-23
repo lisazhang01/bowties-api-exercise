5ae9c2192719bab4c368dd0b5b8630a61c23c789
Lisa Zhang <lisa.zhang231@gmail.com>
2016-10-23 17:54:51 +0800

    Create new bowtie modal

--- app/assets/javascripts/bowties.js
+++ app/assets/javascripts/bowties.js
@@ -2,40 +2,52 @@ $(document).ready(function (){
 
 //Render all bowties
   var generateIndex = function (bowties) {
-    var newElem = "";
+    var bowtieElem = "";
 
     for (var i = 0; i < bowties.length; i++) {
-      newElem += '<div class="col-sm-6 col-md-4 menu"><a href="#" class="thumbnail"><img src="" alt=""><div class="caption">' +
+
+      bowtieElem += '<div class="col-sm-6 col-md-4 menu"><a href="#" class="thumbnail"><div class="caption">' +
         '<h3>' +
         bowties[i].material +
         ' - ' +
         bowties[i].pattern +
         '</h3>' +
-        '<p>...</p><div class="profile hide"><ul><li>'+bowties[i].style +
-        '</li><li> $ ' +
+        '<p>Open to see more details</p><div class="profile hide"><img class="img-responsive img-rounded"src="' +
+        bowties[i].image_url +
+        '" alt=""><ul><li> Style: '+
+        bowties[i].style +
+        '</li><li> Retail Price $ ' +
         bowties[i].retail_price +
+        '</li><li> Wholesale Price $ ' +
+        bowties[i].wholesale_price +
         '</li></ul></div><button type="button" class="btn btn-primary btn-lg more" data-target="#myModal">More</button></div></div></div>';
-
     }
 
-    // allElem = '<div class="row">' + newElem + '</div>';
-
-    $('.bowties').append(newElem);
+    $('.bowties').append(bowtieElem); //Append to index.html
   };
 
 
 //Show details of one bowtie in modal
   function showDetails() {
-    var infoModal = $('#myModal');
+    var infoModal = $('#myModal'); //_modal
     $('.thumbnail').on('click', function(){
-      var htmldata = $(this).find('.profile').html();
-      infoModal.find('.modal-body').html(htmldata);
+
+      var titleData = $(this).find('h3').html();
+      var htmlData  = $(this).find('.profile').html();
+      // var btnOne    = "Edit";
+      // var btnTwo    = "Delete";
+
+      infoModal.find('.modal-title').html(titleData);
+      infoModal.find('.modal-body').html(htmlData);
+      // infoModal.find('.first').text(btnOne);
+      // infoModal.find('.second').text(btnTwo);
       infoModal.modal('show');
       return false;
+
     })
   };
 
-//Get json of all bowties from /api/bowties and render in index
+//Get json of bowties from /api/bowties and render in index
   $.ajax({
     url: '/api/bowties',
     method: 'GET'
@@ -44,6 +56,20 @@ $(document).ready(function (){
     showDetails();
   });
 
+//Create new bowtie
+  function newBowtieForm() {
+    var newModal = $('#myModalCreate'); //_modalCreate
+
+    $('#new-bowtie').on('click', function(e){
+      e.preventDefault();
+
+      newModal.modal('show');
+      return false;
+    })
+
+  }
+
+newBowtieForm();
 
 
 });
